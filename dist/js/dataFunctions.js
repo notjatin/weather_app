7fb7b9caeeb535d29b0251443c147c96eb9223ad
happy <happy@kali.kali>
2024-06-15 18:10:10 +0530

    first commit

---
+++ dist/js/dataFunctions.js
@@ -0,0 +1,49 @@
+export const setLocationObject = (locationObj, coordsObj) => {
+  const { lat, lon, name, unit } = coordsObj;
+  locationObj.setLat(lat);
+  locationObj.setLon(lon);
+  locationObj.setName(name);
+  if (unit) {
+    locationObj.setUnit(unit);
+  }
+}
+
+export const getHomeLocation = () => {
+  return localStorage.getItem("defaultWeatherLocation");
+}
+
+export const getWeatherFromCoords = async (locationObj) => {
+  const lat = locationObj.getLat();
+  const lon = locationObj.getLon();
+  const units = locationObj.getUnit();
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${WEATHER_API_KEY}`;
+  try {
+    const weatherStream = await fetch(url);
+    const weatherJson = await weatherStream.json();
+    return weatherJson;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export const getCoordsFromApi = async (entryText, units) => {
+  const regex = /^\d+$/g;
+  const flag = regex.test(entryText) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+  // `https://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=1ae86ee1684d7bbe174c40d82a396bdf`
+  const encodedUrl = encodeURI(url);
+  try {
+    const dataStream = await fetch(encodedUrl);
+    const jsonData = await dataStream.json();
+    return jsonData;
+  } catch (e) {
+    console.error(e.stack);
+  }
+}
+
+export const cleanText = (text) => {
+  const regex = / {2,}/g;
+  const entryText = text.replaceAll(regex, " ").trim();
+  return entryText;
+}
